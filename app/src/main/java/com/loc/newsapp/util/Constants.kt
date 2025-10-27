d9298cf0e2817928c57fbd01e2b19d72da4e97a8
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-10-27 13:40:56 -0500

    HomeScreen and OnboardingScreen development complete

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.loc.newsapp.util
+
+object Constants {
+    const val USER_SETTINGS = "userSettings"
+
+    const val APP_ENTRY = "appEntry"
+
+    const val API_KEY = "d84b4379313d4ee88f7362d1c0f1a122"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+}
